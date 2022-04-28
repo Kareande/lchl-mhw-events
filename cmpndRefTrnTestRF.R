b07862b15d696726e161a3529c02303d836a5c3f
Kareande <kareande@ucsc.edu>
2022-04-28 12:32:26 -0700

    Corrected refined mtry and min_n values based on the debugged exploratory training

--- cmpndRefTrnTestRF.R
+++ cmpndRefTrnTestRF.R
@@ -15,32 +15,32 @@ n_min_range <- c()
 n_max_range <- c()
 mtry_min_range <- c()
 mtry_max_range <- c()
-n_min_range[1] <- 1 #2 days lag
-n_max_range[1] <- 6
-mtry_min_range[1] <- 26
-mtry_max_range[1] <- 28
+n_min_range[1] <- 5 #2 days lag
+n_max_range[1] <- 12
+mtry_min_range[1] <- 12
+mtry_max_range[1] <- 14
 n_min_range[2] <- 1 #7 days lag
-n_max_range[2] <- 9
-mtry_min_range[2] <- 5
-mtry_max_range[2] <- 9
+n_max_range[2] <- 6
+mtry_min_range[2] <- 9
+mtry_max_range[2] <- 10
 n_min_range[3] <- 1 #14 days lag
-n_max_range[3] <- 5
-mtry_min_range[3] <- 6
+n_max_range[3] <- 8
+mtry_min_range[3] <- 12
 mtry_max_range[3] <- 14
 n_min_range[4] <- 1 #180 days lag
-n_max_range[4] <- 8
-mtry_min_range[4] <- 7
-mtry_max_range[4] <- 10
+n_max_range[4] <- 7
+mtry_min_range[4] <- 11
+mtry_max_range[4] <- 12
 n_min_range[5] <- 1 #365 days lag
-n_max_range[5] <- 5
-mtry_min_range[5] <- 3
-mtry_max_range[5] <- 7
-n_min_range[6] <- 1 #730 days lag
-n_max_range[6] <- 7
-mtry_min_range[6] <- 11
-mtry_max_range[6] <- 16
+n_max_range[5] <- 10
+mtry_min_range[5] <- 7
+mtry_max_range[5] <- 11
+n_min_range[6] <- 10 #730 days lag
+n_max_range[6] <- 14
+mtry_min_range[6] <- 13
+mtry_max_range[6] <- 14
 
-vars_lag = c(2, 7, 14, 180, 365, 730) #2 days, 1 wk, 2 wk, 6 mo
+vars_lag = c(2, 7, 14, 180, 365, 730) #2 days, 1 wk, 2 wk, 6 mo, 1 yr, 2 yr
 n_trees <- 200 #designate number of trees
 final_rfs <- c(rep(NA, 4))
 set.seed(3939)
@@ -48,11 +48,14 @@ doParallel::registerDoParallel(32)
 for(i in 1:length(vars_lag)){
     file_name <- gsub(" ", "", paste("cmpnd_blncd_",vars_lag[i],"lag.csv")) #create dyamic df name
     workingset <- read.csv(gsub(" ", "", paste("cmpndData/",file_name)),sep=",") #get cmpnd df
-    workingset <- workingset[,-c(1:2,31:32)] #remove day, mo, lchlCat, and mhwCat columns
+    workingset <- workingset[ , ! names(workingset) %in% 
+                             c("day","mo","lchlCat","mhwCat", #remove day, mo, lchlCat, and mhwCat columns
+                               "nit","oxy","pho","chl","sil","npp", #remove unlagged lchl vars
+                               "qnet","slp","sat","wndsp","sst","sstRoC")] #remove unlagged mhw vars
     workingset[workingset$cmpCat == 3,]$cmpCat="Compound"
     workingset[workingset$cmpCat == 2,]$cmpCat="LChl Event"
     workingset[workingset$cmpCat == 1,]$cmpCat="MHW Event"
-    workingset[workingset$cmpCat == 0,]$cmpCat="No event"
+    workingset[workingset$cmpCat == 0,]$cmpCat="No Event"
     workingset$cmpCat = as.factor(workingset$cmpCat)
     
     # Split data into training and testing sets
@@ -60,6 +63,8 @@ for(i in 1:length(vars_lag)){
     cmp_train <- training(cmp_split)
     cmp_test <- testing(cmp_split)
     cmp_rec <- recipe(cmpCat ~ ., data = cmp_train)
+    cmp_prep <- prep(cmp_rec)
+    juiced <- juice(cmp_prep)
     
     # Create model specification
     tune_spec <- rand_forest(
@@ -103,10 +108,9 @@ for(i in 1:length(vars_lag)){
     final_rf <- finalize_model(
       tune_spec,
       best_rf)
+    final_rfs[i] <- final_rf
 
     # Check variable importance for training data
-    cmp_prep <- prep(cmp_rec)
-    juiced <- juice(cmp_prep)
     pdf(gsub(" ", "", paste("cmpndFigs/VarImpTrnCmpnd",vars_lag[i],"Lag",n_trees,"T.pdf")))
     pltimg <- final_rf %>%
       set_engine("ranger", importance = "permutation") %>%
@@ -117,15 +121,12 @@ for(i in 1:length(vars_lag)){
     dev.off()
     
     # Use testing data in model
-    cmp_rec <- recipe(cmpCat ~ ., data = cmp_test)
     final_wf <- workflow() %>%
       add_recipe(cmp_rec) %>%
       add_model(final_rf)
     final_res <- final_wf %>%
       last_fit(cmp_split)
-    final_res %>%
-      collect_metrics()
-    
+
     # Produce confusion matrix
     pdf(gsub(" ", "", paste("cmpndFigs/confMatCmpnd",vars_lag[i],"Lag",n_trees,"T.pdf")))
     pltimg <- final_res %>%
@@ -176,7 +177,7 @@ for(i in 1:length(vars_lag)){
          fmt.key="%.3f")
     print(pltimg)
     dev.off()
-    rm(list= ls()[!(ls() %in% c('n_min_range','n_max_range','vars_lag','n_trees','final_rfs'))])
+    #rm(list= ls()[!(ls() %in% c('n_min_range','n_max_range','mtry_min_range','mtry_max_range','vars_lag','n_trees','final_rfs'))])
     }
 doParallel::stopImplicitCluster()
 final_rfs
